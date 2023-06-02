c8b97e74d90f7741ffd696bc3eced29bd2d383f2
William Stoy <ws2574@columbia.edu>
2023-06-02 19:00:58 -0400

    added error state and changed variable types

--- ModbusInterface.cpp
+++ ModbusInterface.cpp
@@ -5,25 +5,30 @@
 #include <ModbusInterface.h>
 
 // constructor
-ModbusInterface::ModbusInterface(HardwareSerial& serial, bool verbose) : _verbose(verbose), _serial(serial) {}
+ModbusInterface::ModbusInterface(HardwareSerial& serial, bool verbose) : _verbose(verbose), _serial(serial) {
+  _inErrorState = false;
+}
 
 void ModbusInterface::begin(int RS485baudrate, int RS485config) {
   // Calculate preDelay and postDelay in microseconds for stable RS-485 transmission
-  bitduration = 1.f / RS485baudrate;
-  wordlen = 10.0f;  // OR 10.0f depending on the channel configuration
-  preDelayBR = bitduration * wordlen * 3.5f * 1e6;
-  postDelayBR = bitduration * wordlen * 3.5f * 1e6;
+  _bitduration = 1.f / RS485baudrate;
+  _wordlen = 10.0f;  // OR 10.0f depending on the channel configuration
+  _preDelayBR = _bitduration * _wordlen * 3.5f * 1e6;
+  _postDelayBR = _bitduration * _wordlen * 3.5f * 1e6;
 
   // Start the Modbus RTU client
-  RS485.setDelays(preDelayBR, postDelayBR);
+  RS485.setDelays(_preDelayBR, _postDelayBR);
 
   if (!ModbusRTUClient.begin(RS485baudrate, RS485config)) {
       _serial.println("Failed to start Modbus RTU Client!");
-      while (1);
-  }
+      
+      _inErrorState = true;
 
-  if (_verbose) {
-    _serial.println("Successfully started Modbus RTU Client!");
+      while (1);
+  } else {
+    if (_verbose) {
+      _serial.println("Successfully started Modbus RTU Client!");
+    }
   }
 }
 
@@ -33,26 +38,32 @@ void ModbusInterface::begin(int RS485baudrate, int RS485config) {
   Writes Holding Register values to the server under specified address.
 */
 bool ModbusInterface::writeHoldingRegisterValues(int address, int startingRegisterAddress, uint8_t *data, int dataLength) {
-    // Set the Holding Register values to counter
-    _serial.print("Writing Holding Registers values ... ");
+  // Set the Holding Register values to counter
+  _serial.print("Writing Holding Registers values ... ");
 
-    // NB: We are decrementing the starting register address by 1 here
-    //     see p.33 of ALICAT OPerating Manual for standard flow and pressure devices (link at top)
-    ModbusRTUClient.beginTransmission(address, HOLDING_REGISTERS, startingRegisterAddress, dataLength);
+  // NB: We are decrementing the starting register address by 1 here
+  //     see p.33 of ALICAT OPerating Manual for standard flow and pressure devices (link at top)
+  ModbusRTUClient.beginTransmission(address, HOLDING_REGISTERS, startingRegisterAddress, dataLength);
 
-    // Write the data out byte-by-byte
-    for (int i = 0; i < dataLength; i++) {
-      ModbusRTUClient.write(data[i]);
-    }
+  // Write the data out byte-by-byte
+  for (int i = 0; i < dataLength; i++) {
+    ModbusRTUClient.write(data[i]);
+  }
 
-    if (!ModbusRTUClient.endTransmission()) {
-      _serial.print("WRITE FAILED! ");
-      _serial.println(ModbusRTUClient.lastError());
-      return false;
-    } else {
-      _serial.println("WRITE SUCCESSFUL");
-      return true;
-    }
+  if (!ModbusRTUClient.endTransmission()) {
+    _serial.print("WRITE FAILED! ");
+    _serial.println(ModbusRTUClient.lastError());
+
+    _inErrorState = true;
+
+    return false;
+  } else {
+    _serial.println("WRITE SUCCESSFUL");
+
+    _inErrorState = false;
+
+    return true;
+  }
 }
 
 
@@ -72,24 +83,29 @@ bool ModbusInterface::writeHoldingRegisterValue(int address, int registerAddress
 /**
   Reads Holding Register values from the server under specified address.
 */
-bool ModbusInterface::readHoldingRegisterValues(int address, int startingRegisterAddress, int nValues, int *response) {
+bool ModbusInterface::readHoldingRegisterValues(int address, int startingRegisterAddress, int nValues, uint8_t *response) {
     _serial.print("Reading Holding Register values ... ");
 
     // NB: we are decrementing the starting register address by 1 here
     //     see p.33 of ALICAT OPerating Manual for standard flow and pressure devices (link at top)
     if (!ModbusRTUClient.requestFrom(address, HOLDING_REGISTERS, startingRegisterAddress, nValues)) {
       _serial.print("READ FAILED! ");
-
       _serial.println(ModbusRTUClient.lastError());
+
+      _inErrorState = true;
+
       return false;
     } else {
       _serial.println("READ SUCCESSFUL");
+
+      _inErrorState = false;
 
       // Process the response message
       int value = 0;
       int responseIndex = 0;
       while (ModbusRTUClient.available()) {
         value = ModbusRTUClient.read();
+        
         // only put a value in the response array if it it is less than the expected number of values
         if (responseIndex < nValues) {
           response[responseIndex] = value;
@@ -106,6 +122,6 @@ bool ModbusInterface::readHoldingRegisterValues(int address, int startingRegiste
 /**
   Read the Holding Register value at a given register from the server at a specified address.
 */
-bool ModbusInterface::readHoldingRegisterValue(int address, int registerAddress, int *response) {
+bool ModbusInterface::readHoldingRegisterValue(int address, int registerAddress, uint8_t *response) {
   return readHoldingRegisterValues(address, registerAddress, 1, response);
 }
--- ModbusInterface.h
+++ ModbusInterface.h
@@ -8,13 +8,14 @@
     private:
       HardwareSerial& _serial; // Use a reference here, not a value
       bool _verbose;
-      float bitduration, wordlen, preDelayBR, postDelayBR;
+      float _bitduration, _wordlen, _preDelayBR, _postDelayBR;
+      bool _inErrorState;
     public:
             ModbusInterface(HardwareSerial& serial, bool verbose);
       void  begin(int RS485baudrate, int RS485config);
       bool  writeHoldingRegisterValues(int address, int startingRegisterAddress, uint8_t *data, int dataLength);
       bool  writeHoldingRegisterValue(int address, int registerAddress, uint8_t dataByte);
-      bool  readHoldingRegisterValues(int address, int startingRegisterAddress, int nValues, int *response);
-      bool  readHoldingRegisterValue(int address, int registerAddress, int *response);
+      bool  readHoldingRegisterValues(int address, int startingRegisterAddress, int nValues, uint8_t *response);
+      bool  readHoldingRegisterValue(int address, int registerAddress, uint8_t *response);
   };
 #endif
